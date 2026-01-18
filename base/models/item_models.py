9b9fcebd68e114bcca5ea9c6d2465a1a5d7cf401
TMando616 <t.mandokoro616@gmail.com>
2026-01-18 16:55:09 +0900

    feat: idカラムの設定

--- base/models/item_models.py
+++ base/models/item_models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.utils.crypto import get_random_string
+
+def create_id():
+    return get_random_string(22)
 
 class Item(models.Model):
+    id = models.CharField(default=create_id, primary_key=True, max_length=22, editable=False) # create_idの後に（）を付けると都度実行される
     name = models.CharField(default='', max_length=50)
     price = models.PositiveIntegerField(default=0)
     stock = models.PositiveIntegerField(default=0)
