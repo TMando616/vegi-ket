1f6041db1562a04555f2afe06afb4fb8fab96edd
TMando616 <t.mandokoro616@gmail.com>
2026-01-26 17:11:03 +0900

    chore: 関数での書き方

--- base/views/item_veiws.py
+++ base/views/item_veiws.py
@@ -7,3 +7,12 @@ class IndexListView(ListView):
     # 基本の項目
     model = Item # Itemの一覧を取得
     template_name = 'pages/index.html' # どのテンプレートを使用するか
+
+
+# # 上記を関数で実装した場合
+# def index(request):
+#     object_list = Item.objects.all()
+#     context = {
+#         'object_list': object_list,
+#     }
+#     return render(request, 'page/index.html', context)
