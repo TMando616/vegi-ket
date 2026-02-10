b1ca380a9ead8c1bab29701f78bb2038bb2a4e60
TMando616 <t.mandokoro616@gmail.com>
2026-02-10 14:41:44 +0900

    feat: AddToCartのPOST処理を追加

--- base/views/cart_views.py
+++ base/views/cart_views.py
@@ -8,7 +8,18 @@ class CartListView(ListView):
     model = Item
     template_name = 'pages/cart.html'
 
-class AddCartView(View): #View 
+class AddCartView(View): #View method毎に処理を分けられる
 
     def post(self, request): # postと定義すると、postのリクエストを受け取ることができる
-        pass
+        item_pk = request.POST.get('item_pk')
+        quantity = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', None)
+        if cart is None or len(cart) == 0:
+            items = OrderedDict()
+            cart = {'items': items}
+        if item_pk in cart['items']:
+            cart['items'][item_pk] += quantity
+        else:
+            cart['items'][item_pk] = quantity
+        request.session['cart'] = cart
+        return redirect('/cart/')
