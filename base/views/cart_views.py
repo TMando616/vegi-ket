d308b2912687869d8c7732d46e98dda0ac632b14
TMando616 <t.mandokoro616@gmail.com>
2026-02-11 08:00:50 +0900

    feat: query_set のoverride

--- base/views/cart_views.py
+++ base/views/cart_views.py
@@ -5,8 +5,37 @@ from base.models import Item
 from collections import OrderedDict
 
 class CartListView(ListView):
-    model = Item
+    model = Item #すべてのアイテムを返している→セッション情報などに即したアイテムにしたい
     template_name = 'pages/cart.html'
+
+
+    # query_set：アイテムのそれぞれのオブジェクトのセットのこと
+    # 今回の修正で選択されたカートのみ返すようにしている
+    def get_queryset(self): # ListViewが持つメソッド名、同名でoverrideする
+        cart = self.request.session.get('cart', None)
+
+        if cart is None or len(cart) == 0: # cartがないか0この場合はリダイレクト
+            return redirect('/')
+        
+        # 初期化処理
+        self.qyeryset = []
+        self.total = 0
+
+        # cartの中にはitemsがある前提（OrderedDict形式）
+        for item_pk, quantity in cart['items'].items(): # key:item_pk, value:quantity としてループ処理
+            obj = Item.objects.get(pk=item_pk)
+            obj.quantity = quantity
+            obj.subtotal = int(obj.price * quantity)
+            self.queryset.append(obj)
+            self.total += obj.subtotal
+
+        self.tax_included_total = int(obj.total * (settings.TAX_RATE + 1))
+        cart['total'] = self.total
+        cart['tax_included_total'] = self.tax_included_total
+        self.request.session['cart'] = cart
+
+        return super().get_queryset()
+
 
 class AddCartView(View): #View method毎に処理を分けられる
 
