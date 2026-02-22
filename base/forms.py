0f574d433af98af6d826a181966a85ae02d93298
TMando616 <t.mandokoro616@gmail.com>
2026-02-22 15:52:51 +0900

    feat: forms.pyの作成

---
+++ base/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password', )
+
+    # clean_passwordをoverride
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        return password
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.get_password(self.cleaned_data["password"])
+
+        if commit:
+            user.save()
+
+        return user
