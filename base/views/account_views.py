1f6a7fde8088f6097919e5c2d7613b81140a4dc4
TMando616 <t.mandokoro616@gmail.com>
2026-03-01 09:07:04 +0900

    chore: コメント追記

--- base/views/account_views.py
+++ base/views/account_views.py
@@ -5,11 +5,14 @@ from django.contrib.auth import get_user_model
 from base.models import Profile
 from base.forms import UserCreationForm
 
-
+# 新規登録画面
 class SignUpView(CreateView):
     form_class = UserCreationForm
+
+    # 新規登録作成後のURL
     success_url = '/login/'
     template_name = 'pages/login_signup.html'
 
+    # 親のCreaeViewのOverride
     def form_valid(self, form):
         return super().form_valid(form)
